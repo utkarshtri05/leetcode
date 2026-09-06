8aa7c37db4bbee27d0a59c4e6bf7860b65f61414
Utkarsh Tripathi <utkarshtripathi3105@gmail.com>
2026-09-06 23:48:52 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -5,13 +5,11 @@ class Solution {
         }
             int a=0;
             int b=1;
-        
             for(int i=2;i<=n;i++){
                 int next=a+b;
                 a=b;
                 b=next;
             }
-            return b;
-        
+            return b; 
     }
 }
