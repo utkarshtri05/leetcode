4fb21f0972043ea2655ac2f2cfc324233db4a8f4
Utkarsh Tripathi <utkarshtripathi3105@gmail.com>
2026-09-12 21:52:11 +0530

    Time: 1 ms (97.44%), Space: 42.9 MB (14.53%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,11 +1,40 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        
-        long n=dividend/divisor;
-        // if(n<Integer.MIN_VALUE) return 
-        if(dividend<=-2147483648 && divisor==1) return -2147483648;
-        if(dividend<=-2147483648 && divisor==-1) return 2147483647;
-        return (int) n;
+        long a = dividend;
+        long b = divisor;
+
+        boolean negative = (a < 0) ^ (b < 0);
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        long quotient = 0;
+
+        while (a >= b) {
+            long currentDivisor = b;
+            long multiple = 1;
+
+            while (a >= currentDivisor + currentDivisor) {
+                currentDivisor += currentDivisor;
+                multiple += multiple;
+            }
+
+            a -= currentDivisor;
+            quotient += multiple;
+        }
+
+        if (negative) {
+            quotient = -quotient;
+        }
+
+        if (quotient > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (quotient < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        return (int) quotient;
     }
 }
- 
